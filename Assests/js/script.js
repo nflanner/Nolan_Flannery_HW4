298971b44533970b81a9dbfb68252f013b7568de
Nolan Flannery <nflannery@blueorigin.com>
2022-04-28 09:43:33 -0700

    added comments

--- Assests/js/script.js
+++ Assests/js/script.js
@@ -19,6 +19,7 @@ var points = 0;
 var finished = false;
 var highScores = [];
 
+// upon loading the url, load locally-saved high scores and set the default starting page
 loadScores();
 setDefault();
 
@@ -29,9 +30,11 @@ function setTime() {
             secondsLeft--;
             timeEl.textContent = 'Time: ' + secondsLeft;
         }
+        // clear the interval if the quiz is finished
         if (finished === true) {
             clearInterval(timeInterval);
         }
+        // end the quiz if the timer reaches zero but the user hasn't finished
         if (secondsLeft === 0 && finished === false) {
             clearUL();
             finishedScreen();
@@ -39,6 +42,7 @@ function setTime() {
     }, 1000);
 };
 
+// this sets the initial screen when first loading in to the quiz
 function setDefault() {
     secondsLeft = 75;
     timeEl.textContent = "Time: 0"
@@ -51,11 +55,13 @@ function setDefault() {
     points = 0;
 };
 
+// this reverts the default screen
 function revertDefault() {
     startText.textContent = "";
     clearStartEl();
 };
 
+// helper function for creating list elemtns that contain buttons for the answer options
 function newButtonList(buttonContent, currentIndex) {
     var li = document.createElement("li");
     li.setAttribute("data-index", currentIndex);
@@ -63,6 +69,7 @@ function newButtonList(buttonContent, currentIndex) {
     totalList.appendChild(li);
 }
 
+// this function sets all the questions and answers
 function setQuestion(currentIndex) {
     bigText.textContent = Object.keys(questionAnswers)[currentIndex];
     var i = 0;
@@ -72,12 +79,14 @@ function setQuestion(currentIndex) {
     }
 };
 
+// records the outcome of each round,tells the user if the answer was wrong or right, and records the points
 function recordRound() {
     var outcome = roundWon ? 'Correct!' : 'Wrong!';
     startEl.textContent = outcome;
     points += roundWon ? 10 : 0;
 }
 
+// helper function that sets the screen directly after finishing the quiz
 function finishedScreen() {
     bigText.textContent = "All Done!"
     startText.textContent = 'Your final score was ' + points + '.';
@@ -89,6 +98,7 @@ function finishedScreen() {
     totalList.appendChild(li);
 }
 
+// this saves the initals and score to local storage
 function saveScore() {
     var initials = document.getElementsByTagName("input")[0].value;
     highScores.push({
@@ -98,6 +108,7 @@ function saveScore() {
     localStorage.setItem("highScores", JSON.stringify(highScores));
 }
 
+// loads the locally sctored scores so that refreshing the page doesn't lose any data
 function loadScores() {
     var localScoreList = JSON.parse(localStorage.getItem("highScores"));
     if (localScoreList != null) {
@@ -107,6 +118,7 @@ function loadScores() {
     }
 }
 
+// helper function that sets the high score page
 function highScorePage() {
     bigText.textContent = 'High Scores';
     startText.textContent = "";
@@ -119,6 +131,7 @@ function highScorePage() {
     startEl.appendChild(createElement("button", "Clear"));
 }
 
+// this fully clears the unordered list element
 function clearUL() {
     var length = totalList.children.length;
     for (var i = 0; i < length; i++) {
@@ -126,6 +139,7 @@ function clearUL() {
     }
 }
 
+// this fully clears the start div element
 function clearStartEl () {
     var length = startEl.children.length;
     for (var i = 0; i < length; i++) {
@@ -133,6 +147,7 @@ function clearStartEl () {
     }
 }
 
+// helper function that creates any given element and sets text content/attribute
 function createElement(type, content, attr, attrContent) {
     var element = document.createElement(type);
     element.textContent = content;
@@ -140,21 +155,26 @@ function createElement(type, content, attr, attrContent) {
     return element;
 }
 
+// this event listener is for the ul element 
 totalList.addEventListener("click", function(event) {
     event.preventDefault();
     var element = event.target;
 
+    // if the button is selected and the text content of that button is not 'Submit' then the user selected an answer
     if (element.matches('button') && element.textContent !== 'Submit') {
         var index = element.parentElement.getAttribute("data-index");
+        // validate that the index associated with the selected answer is the same as the index of the correct answer
         if (index == answerKey[currentIndex]) {
             roundWon = true;
         } else {
             roundWon = false;
             secondsLeft -= 10;
         }
+        // the question answered is now complete - record the points then clear the answers
         recordRound();
         clearUL();
         currentIndex++;
+        // make sure all questions have or have not been answered, then set a new question or go to the finish screen
         if (currentIndex < answerKey.length) {
             setQuestion(currentIndex);
         } else {
@@ -162,26 +182,31 @@ totalList.addEventListener("click", function(event) {
         }
     }
 
+    // if the user submits there score then save the score locally and then activate the high score page
     if (element.textContent === "Submit") {
         saveScore();
         highScorePage();
     }
 });
 
+// this event listener looks for 'Back', 'Clear', and 'Start Quiz' button selections
 startEl.addEventListener("click", function(event){
     event.preventDefault();
     var element = event.target;
 
+    // returns to start page
     if (element.textContent === 'Back') {
         setDefault();
     }
 
+    // clears the locally-stored high scores
     if (element.textContent === 'Clear') {
         highScores = [];
         localStorage.setItem("highScores", JSON.stringify(highScores));
         clearUL();
     }
 
+    // starts the quiz again
     if (element.textContent === 'Start Quiz') {
         finished = false;
         setTime();
